b768f9287bca790179ab45c88d1e7709bfd233b9
P-Russell <pdrussell011@gmail.com>
2018-06-18 21:33:57 +0200

    cast to main durin gtests

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,6 +3,7 @@
  * 2. Create instance of operand
 */
 
+#include <cstdlib>
 #include "Operand.tmp.hpp"
 
 int main ()
@@ -10,6 +11,14 @@ int main ()
 	Operand<int> *i = new Operand<int>(56);
 
 	std::cout << i->getPrecision() << " " << i->getValue() << std::endl;
+	
+	Operand<int8_t> *k = new Operand<int8_t>(127);
+
+	std::cout << k->getPrecision() << " " << static_cast<int>(k->getValue()) << std::endl;
+	
+	Operand<int32_t> *j = new Operand<int32_t>(8);
+
+	std::cout << j->getPrecision() << " " << j->getValue() << std::endl;
 	
 	return 0;
 }
