338a95cfce779c33db28c84642f5310414a731d0
Patrick RUSSELL <prussell@e4r5p2.wethinkcode.co.za>
2018-06-19 16:07:04 +0200

    still working on template

--- includes/Operand.tmp.hpp
+++ includes/Operand.tmp.hpp
@@ -1,17 +1,18 @@
 #include <iostream>
 
-template <typename T>
+template <class T>
 class Operand {
 
 public:
 	Operand<T>(void);
 	Operand<T>(T value): _value(value), _precision(sizeof(value)) {}
-	~Operand<T>(void) { delete this; }
-	//Operand & operator=(const & Operand rhs);
+	~Operand<T>(void); 
+	Operand & operator=(const Operand<T> & rhs);
+	Operand<T>(const Operand & rhs) : _value(rhs._value), _precision(rhs._precision) {}
 	int getPrecision(void) { return this->_precision; }
 	T getValue(void) { return _value; }
 private:
-	T _value;
+	const T _value;
 	const int _precision;
 };
 
