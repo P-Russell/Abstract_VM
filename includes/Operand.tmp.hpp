5421a5aa5d1139f3f3c88f1971768847a35a8f06
P-Russell <pdrussell011@gmail.com>
2018-06-21 13:17:27 +0200

    Implements interface

--- includes/Operand.tmp.hpp
+++ includes/Operand.tmp.hpp
@@ -1,18 +1,60 @@
 #include <iostream>
+#include <sstream>
+#include "IOperand.hpp"
 
 template <class T>
-class Operand {
+class Operand : public IOperand {
 
 public:
 	Operand<T>(void);
 	Operand<T>(T value): _value(value), _precision(sizeof(value)) {}
-	~Operand<T>(void); 
-	Operand & operator=(const Operand<T> & rhs);
-	Operand<T>(const Operand & rhs) : _value(rhs._value), _precision(rhs._precision) {}
-	int getPrecision(void) { return this->_precision; }
+	~Operand<T>(void) {} 
+	Operand & operator=(const Operand<T> & rhs) {
+		this->_value = rhs._value;
+		this->_precision = rhs._precision;
+		return *this;
+	};
+	Operand<T>(const Operand & rhs) : _value(rhs._value), _precision(rhs._precision) {
+	}
+
+	int getPrecision(void) const { return this->_precision; }
+
 	T getValue(void) { return _value; }
+
+
+	IOperand const * operator + (IOperand const & rhs) const {
+		(void)rhs;
+		return this;
+	}
+	IOperand const * operator - (IOperand const & rhs) const {
+		(void)rhs;
+		return this;
+	}
+	IOperand const * operator * (IOperand const & rhs) const {
+		(void)rhs;
+		return this;
+	}
+	IOperand const * operator / (IOperand const & rhs) const {
+		(void)rhs;
+		return this;
+	}
+	IOperand const * operator % (IOperand const & rhs) const {
+		(void)rhs;
+		return this;
+	}
+
+	std::string const & toString(void) const
+	{
+		std::stringstream ss;
+		ss << "Address: " << this;
+		ss << " Value: " << this->_value;
+		ss << " Precision: " << this->_precision;
+		const std::string *ret = new std::string(ss.str());
+		return *ret;
+	}
+
 private:
-	const T _value;
-	const int _precision;
+	T _value;
+	int _precision;
 };
 
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,6 +10,8 @@ int main ()
 {
 	Operand<int> *i = new Operand<int>(56);
 
+	Operand<int> cpy = *i;
+
 	std::cout << i->getPrecision() << " " << i->getValue() << std::endl;
 	
 	Operand<int8_t> *k = new Operand<int8_t>(127);
@@ -19,6 +21,31 @@ int main ()
 	Operand<int32_t> *j = new Operand<int32_t>(8);
 
 	std::cout << j->getPrecision() << " " << j->getValue() << std::endl;
+
+	
+	std::cout << "-------------Copy Constructor--------------" << std::endl;
+
+	Operand<int> rig(32);
+	Operand<int> lef = rig;
+
+	std::cout << "Rig:  " << rig.getPrecision() << " " << rig.getValue() << std::endl;
+	std::cout << "Lef:  " << lef.getPrecision() << " " << lef.getValue() << std::endl;
+
+	(void)rig;
+	(void)lef;
+
+	std::cout << "-------------Assignment Constructor--------------" << std::endl;
+
+	Operand<int> rig1(32);
+	Operand<int> lef1(24);
+
+	lef1 = rig1;
+
+	std::cout << "Rig:  " << rig1.getPrecision() << " " << rig1.getValue() << std::endl;
+	std::cout << "Lef:  " << lef1.getPrecision() << " " << lef1.getValue() << std::endl;
+
+	(void)rig1;
+	(void)lef1;
 	
 	return 0;
 }
