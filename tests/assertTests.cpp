8b3371565629eb15961dc9f4f01e8cad114895b6
Patrick RUSSELL <prussell@e4r5p2.wethinkcode.co.za>
2018-07-03 13:48:01 +0200

    added assert test cases

---
+++ tests/assertTests.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <climits>
+#include "Operand.tmp.hpp"
+#include "eOperandType.hpp"
+#include "Factory.hpp"
+#include "Stack.hpp"
+#include "gtest/gtest.h"
+
+TEST(assertFloat, types1)
+{
+	Factory fa;
+    Stack stack;
+	eOperandType f(eOperandType::FLOAT);
+	eOperandType d(eOperandType::DOUBLE);
+
+    stack.push(fa.createOperand(f, "5"));
+    stack.push(fa.createOperand(d, "2"));
+    stack.divide();
+	EXPECT_TRUE(stack.assertThat(fa.createOperand(d, "2.5")));
+}
+
+TEST(assertThrow, types1)
+{
+	Factory fa;
+    Stack stack;
+	eOperandType f(eOperandType::FLOAT);
+	eOperandType d(eOperandType::DOUBLE);
+
+    stack.push(fa.createOperand(d, "10"));
+    EXPECT_THROW(stack.assertThat(fa.createOperand(f, "10")), Error::assert_fail);
+    EXPECT_THROW(stack.assertThat(fa.createOperand(eOperandType::INT_16, "10")), Error::assert_fail);
+    EXPECT_TRUE(stack.assertThat(fa.createOperand(d, "10")));
+}
